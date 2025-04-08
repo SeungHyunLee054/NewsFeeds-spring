85762076d7d518ac4f7251d07e6600a4c4172224
chat26666 <chat26666@gmail.com>
2025-04-08 15:31:26 +0900

    Feat : 좋아요 추가 취소 확인 기능 추가

---
+++ src/main/java/com/nbc/newsfeeds/domain/heart/controller/HeartController.java
@@ -0,0 +1,60 @@
+package com.nbc.newsfeeds.domain.heart.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nbc.newsfeeds.domain.heart.dto.HeartResponseDto;
+import com.nbc.newsfeeds.domain.heart.service.HeartService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/feeds/{feedId}/likes")
+@RequiredArgsConstructor
+public class HeartController {
+
+	private final HeartService heartService;
+
+	@PostMapping
+	@Operation(summary = "좋아요 추가", security = {@SecurityRequirement(name = "bearer")})
+	public ResponseEntity<HeartResponseDto> addHeart(
+		@AuthenticationPrincipal long memberId,
+		@PathVariable long feedId
+	) {
+		heartService.addHeart(memberId, feedId);
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(heartService.viewHeart(feedId));
+	}
+
+	@DeleteMapping
+	@Operation(summary = "좋아요 취소", security = {@SecurityRequirement(name = "bearer")})
+	public ResponseEntity<HeartResponseDto> cancelHeart(
+		@AuthenticationPrincipal long memberId,
+		@PathVariable long feedId
+	) {
+		heartService.cancelHeart(memberId, feedId);
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(heartService.viewHeart(feedId));
+	}
+
+	@GetMapping
+	@Operation(summary = "좋아요 확인")
+	public ResponseEntity<HeartResponseDto> viewHeart(
+		@PathVariable long feedId
+	) {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(heartService.viewHeart(feedId));
+	}
+}
---
+++ src/main/java/com/nbc/newsfeeds/domain/heart/service/HeartServiceImpl.java
@@ -0,0 +1,59 @@
+package com.nbc.newsfeeds.domain.heart.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.nbc.newsfeeds.domain.feed.entity.Feed;
+import com.nbc.newsfeeds.domain.heart.dto.HeartResponseDto;
+import com.nbc.newsfeeds.domain.heart.entity.Heart;
+import com.nbc.newsfeeds.domain.heart.repository.HeartRepository;
+import com.nbc.newsfeeds.domain.member.entity.Member;
+import com.nbc.newsfeeds.domain.member.repository.MemberRepository;
+import com.nbc.newsfeeds.domain.feed.repository.FeedRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class HeartServiceImpl implements HeartService {
+
+	private final HeartRepository heartRepository;
+	private final MemberRepository memberRepository;
+	private final FeedRepository feedRepository;
+
+	@Transactional
+	public void addHeart(long memberId, long feedId) {
+		if (!heartRepository.findByMember_IdAndFeed_Id(memberId, feedId)) {
+			Member member = memberRepository.findById(memberId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 사용자가 존재하지 않습니다."));
+			Feed feed = feedRepository.findById(feedId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 피드가 조회되지 않습니다."));
+			Heart heart = Heart.builder()
+				.feed(feed)
+				.member(member)
+				.build();
+			heartRepository.save(heart);
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 사용자가 피드에 좋아요를 이미 남겼습니다.");
+		}
+	}
+
+	@Transactional
+	public void cancelHeart(long memberId, long feedId) {
+		if (!heartRepository.findByMember_IdAndFeed_Id(memberId, feedId)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 사용자가 피드에 좋아요를 남긴 기록이 없습니다.");
+		} else {
+			heartRepository.deleteByMember_IdAndFeed_Id(memberId, feedId);
+			// FIXME : new speed 의 heart_count 를 감소하는 로직 구현 필요
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public HeartResponseDto viewHeart(long feedId) {
+		return new HeartResponseDto(heartRepository.countByFeed_Id(feedId));
+		// FIXME : new speed 의 heart_count 를 읽어오는 로직 구현 필요
+	}
+
+}
