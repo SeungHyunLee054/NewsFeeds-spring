6e70344d18d30754d07ef4538cee2b3d9154c97a
dungbik <q1w2e30630@gmail.com>
2025-04-08 10:08:32 +0900

    Refactor : CursorPage 를 record 로 변환

--- src/main/java/com/nbc/newsfeeds/domain/friend/model/response/CursorPage.java
+++ src/main/java/com/nbc/newsfeeds/domain/friend/model/response/CursorPage.java
@@ -3,18 +3,13 @@ package com.nbc.newsfeeds.domain.friend.model.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class CursorPage {
+public record CursorPage(
 	@Schema(description = "다음 페이지에서 사용할 커서")
-	private Long nextCursor;
+	Long nextCursor,
 
 	@Schema(description = "다음 페이지 여부")
-	private Boolean hasNext;
+	Boolean hasNext
+) {
 }
