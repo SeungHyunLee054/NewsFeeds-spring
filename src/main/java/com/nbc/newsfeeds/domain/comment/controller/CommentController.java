4508ea647529d9a3f9fddfc59f1f89a6f7ed4456
Uguls <chobozaex@gmail.com>
2025-04-08 10:23:40 +0900

    feat:  Valid 적용
    컨트롤러, DTO Valid 적용

--- src/main/java/com/nbc/newsfeeds/domain/comment/controller/CommentController.java
+++ src/main/java/com/nbc/newsfeeds/domain/comment/controller/CommentController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,48 +21,50 @@ import com.nbc.newsfeeds.domain.comment.dto.request.CommentUpdateRequest;
 import com.nbc.newsfeeds.domain.comment.dto.response.CommentResponse;
 import com.nbc.newsfeeds.domain.comment.service.CommentService;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/comments")
 @RequiredArgsConstructor
+@Validated
 public class CommentController {
 
 	private final CommentService commentService;
 
 	@PostMapping()
 	public ResponseEntity<CommentResponse> createComment(
-		@RequestParam Long feedId,
-		@RequestBody CommentCreateRequest create
+		@RequestParam @Positive Long feedId,
+		@Valid @RequestBody CommentCreateRequest create
 	) {
-		//TODO Validation 400 Bad Request
 		return new ResponseEntity<>(commentService.createComment(feedId, create), HttpStatus.CREATED);
 	}
 
 	@GetMapping()
 	public ResponseEntity<CommentResponse> getCommentsByFeedId(
-		@RequestParam Long feedId,
+		@RequestParam @Positive Long feedId,
 		@PageableDefault(size = 10, page = 0, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
 	) {
 		return new ResponseEntity<>(commentService.getCommentsByFeedId(feedId, pageable), HttpStatus.OK);
 	}
 
 	@GetMapping("/{commentId}")
-	public ResponseEntity<CommentResponse> getCommentByFeedId(@PathVariable Long commentId) {
+	public ResponseEntity<CommentResponse> getCommentByFeedId(@PathVariable @Positive Long commentId) {
 		return new ResponseEntity<>(commentService.getCommentById(commentId), HttpStatus.OK);
 	}
 
 	@PutMapping("/{commentId}")
 	public ResponseEntity<CommentResponse> updateComment(
-		@PathVariable Long commentId,
-		@RequestBody CommentUpdateRequest request
+		@PathVariable @Positive Long commentId,
+		@Valid @RequestBody CommentUpdateRequest request
 	) {
-		//TODO Validation 400 Bad Request
 		return new ResponseEntity<>(commentService.updateComment(commentId, request), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{commentId}")
-	public ResponseEntity<CommentResponse> deleteComment(@PathVariable Long commentId) {
+	public ResponseEntity<CommentResponse> deleteComment(@PathVariable @Positive Long commentId) {
 		return new ResponseEntity<>(commentService.deleteByCommentId(commentId), HttpStatus.OK);
 	}
 }
