7c1cf4894b8c4f72003df776925cb546d618592f
dungbik <q1w2e30630@gmail.com>
2025-04-08 14:45:51 +0900

    Fix : BizException 공통 예외에서 Friend 종속 예외로 수정

--- src/main/java/com/nbc/newsfeeds/domain/friend/exception/FriendBizException.java
+++ src/main/java/com/nbc/newsfeeds/domain/friend/exception/FriendBizException.java
@@ -1,17 +1,18 @@
-package com.nbc.newsfeeds.common.exception;
+package com.nbc.newsfeeds.domain.friend.exception;
 
 import org.springframework.http.HttpStatus;
 
+import com.nbc.newsfeeds.common.exception.BaseException;
 import com.nbc.newsfeeds.common.response.ResponseCode;
 
 import lombok.Getter;
 
 @Getter
-public class BizException extends BaseException {
+public class FriendBizException extends BaseException {
 	private final ResponseCode responseCode;
 	private final HttpStatus httpStatus;
 
-	public BizException(ResponseCode responseCode) {
+	public FriendBizException(ResponseCode responseCode) {
 		this.responseCode = responseCode;
 		this.httpStatus = responseCode.getHttpStatus();
 	}
