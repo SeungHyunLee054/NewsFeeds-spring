21d588dff9b0c3c042c8a3b30a978cc3a06e7458
Uguls <chobozaex@gmail.com>
2025-04-08 17:34:04 +0900

    refactor: CommentException 사용
    에러응답을 위해 CommentException 사용

--- src/main/java/com/nbc/newsfeeds/domain/comment/exception/CommentException.java
+++ src/main/java/com/nbc/newsfeeds/domain/comment/exception/CommentException.java
@@ -4,24 +4,17 @@ import org.springframework.http.HttpStatus;
 
 import com.nbc.newsfeeds.common.exception.BaseException;
 import com.nbc.newsfeeds.common.response.ResponseCode;
-import com.nbc.newsfeeds.domain.comment.code.CommentExceptionCode;
 
+import lombok.Getter;
+
+@Getter
 public class CommentException extends BaseException {
 
-	private final CommentExceptionCode code;
+	private final ResponseCode responseCode;
+	private final HttpStatus httpStatus;
 
-	public CommentException(CommentExceptionCode code) {
-		super();
-		this.code = code;
-	}
-
-	@Override
-	public HttpStatus getHttpStatus() {
-		return code.getHttpStatus();
-	}
-
-	@Override
-	public ResponseCode getErrorCode() {
-		return code;
+	public CommentException(ResponseCode responseCode) {
+		this.responseCode = responseCode;
+		this.httpStatus = responseCode.getHttpStatus();
 	}
 }
