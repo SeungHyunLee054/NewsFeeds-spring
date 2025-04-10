a6762df9372c83d81aacf4d2f9b336107dbf5e43
SeungHyunLee054 <yellow13lsh@naver.com>
2025-04-10 17:26:20 +0900

    Style : 불필요한 import 제거
    불필요한 import 제거

--- src/main/java/com/nbc/newsfeeds/domain/member/entity/Member.java
+++ src/main/java/com/nbc/newsfeeds/domain/member/entity/Member.java
@@ -5,11 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import com.nbc.newsfeeds.common.audit.BaseEntity;
-import com.nbc.newsfeeds.domain.member.constant.MemberResponseCode;
-import com.nbc.newsfeeds.domain.member.exception.MemberException;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
