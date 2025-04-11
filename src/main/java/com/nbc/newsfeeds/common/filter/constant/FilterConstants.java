0302e8566710a8b052b0fbc07def0f61afb8eb58
SeungHyunLee054 <yellow13lsh@naver.com>
2025-04-11 11:16:58 +0900

    Refactor : 상수 클래스에 생성자 추가
    임의로 생성되지 않도록 private 생성자 추가

--- src/main/java/com/nbc/newsfeeds/common/filter/constant/FilterConstants.java
+++ src/main/java/com/nbc/newsfeeds/common/filter/constant/FilterConstants.java
@@ -2,6 +2,10 @@ package com.nbc.newsfeeds.common.filter.constant;
 
 import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class FilterConstants {
 	public static final List<String> WHITE_LIST = List.of("/auth/signin", "/auth/signup",
 		"/resources", "/swagger-ui", "/v3/api-docs", "/swagger-resources", "/webjars");
